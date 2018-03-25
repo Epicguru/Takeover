2cb76c074aa0207c1871ced0a9a2bb4b5b9cb88e
James Billy <james@epicguru.co.uk>
2018-03-25 13:23:20 +0200

    Added basic pawn scripts.

---
+++ Assets/Scripts/Pawns/Health.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class Health : NetworkBehaviour
+{
+    [SyncVar]
+    public float CurrentHealth = 100f;
+
+    [SyncVar]
+    public float MaxHealth = 100f;
+
+    public virtual bool IsDead
+    {
+        get
+        {
+            return CurrentHealth <= 0f;
+        }
+    }
+
+    public virtual bool FullHealth
+    {
+        get
+        {
+            return CurrentHealth == MaxHealth;
+        }
+    }
+
+    [Server]
+    public void SetMaxHealth()
+    {
+        if(CurrentHealth < MaxHealth)
+            CurrentHealth = MaxHealth;
+    }
+
+    [Server]
+    public void ChangeHealth(float change)
+    {
+        SetHealth(CurrentHealth + change);
+    }
+
+    [Server]
+    public void SetHealth(float health)
+    {
+        // Cannot go below 0 or above max.
+        CurrentHealth = Mathf.Clamp(health, 0f, MaxHealth);
+    }
+}
---
+++ Assets/Scripts/Pawns/Pawn.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+[RequireComponent(typeof(Health))]
+[RequireComponent(typeof(NetPosSync))]
+public class Pawn : NetworkBehaviour
+{
+    // A pawn is an intelligent creature that can navigate and interact with the world.
+    // Has support for health, pathfinding, that kind of stuff.
+
+    public Health Health
+    {
+        get
+        {
+            if(_Health == null)
+            {
+                _Health = GetComponent<Health>();
+            }
+            return _Health;
+        }
+    }
+    private Health _Health;
+
+    public NetPosSync NetPosSync
+    {
+        get
+        {
+            if (_NetPosSync == null)
+            {
+                _NetPosSync = GetComponent<NetPosSync>();
+            }
+            return _NetPosSync;
+        }
+    }
+    private NetPosSync _NetPosSync;
+}
